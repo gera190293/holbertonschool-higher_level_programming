dedc1442a57361831906a7803a639428df2a330d
gera190293 <gera190293@gmail.com>
2024-01-24 12:21:07 -0800

    Python - if/else, loops, functions

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(0,100):
+for n in range(00,100):
     if n != 99:
-        print("{}, ".format(n), end = "")
+        print("{:02d}, ".format(n), end = "")
     else:
-        print("{}".format(n))
+        print("{:02d}".format(n))
