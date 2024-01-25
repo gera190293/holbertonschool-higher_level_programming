9d59b50b124b8eb5bfe0c7ad62bf0126bee86716
gera190293 <gera190293@gmail.com>
2024-01-25 13:32:35 -0800

    Python - if/else, loops, functions

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
         if (n%3 == 0 and n%5 != 0):
             print("Fizz ", end="")
         elif (n%3 != 0 and n%5 == 0):
-            print("Fuzz ", end="")
+            print("Buzz ", end="")
         elif (n%3 == 0 and n%5 == 0):
-            print("FizzFuzz ", end="")
+            print("FizzBuzz ", end="")
         else:
             print("{} ".format(n), end="")
