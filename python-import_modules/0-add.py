dcebb9067d5ebf0ce39aedd680cceb2449846bb5
gera190293 <gera190293@gmail.com>
2024-01-25 15:04:59 -0800

    Python - import & modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import add_0
-def main:
+def main():
     a = 1
     b = 2
     c = add_0.add(a, b)
     print("{} + {} = {}".format(a, b, c))
+if __name__ == "__main__":
+    main()
