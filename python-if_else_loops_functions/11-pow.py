bcdf506844c72f9814bb8c531b8363b1cf908c4b
gera190293 <gera190293@gmail.com>
2024-01-25 13:50:58 -0800

    Python - if/else, loops, functions

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
-    m = 1
-    for n in range(b):
-        m *= a
-    return m
+    if b == 0:
+        return 1
+    elif b > 0:
+        m = 1
+        for n in range(b):
+            m *= a
+        return m
+    else:
+        for n in range(abs(b)):
+            m /= a
+        return m
