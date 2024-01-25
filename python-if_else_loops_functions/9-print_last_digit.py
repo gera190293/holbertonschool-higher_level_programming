062aa9fd34c119bd65b8908ea96337e722a688cd
gera190293 <gera190293@gmail.com>
2024-01-25 12:59:58 -0800

    Python - if/else, loops, functions

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(number[-1:])
+    if number < 0:
+        print("{}".format(abs(number) % 10))
+    else:
+        print("{}".format(number % 10))
