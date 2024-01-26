ae2ca9aefca9d7ae1c6b67962770147442ec75d3
gera190293 <gera190293@gmail.com>
2024-01-26 17:12:05 -0300

    Python - if/else, loops, functions

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+
+def getUpperChar(c):
+    if ord(n) >= 97 and ord(n) <= 122:
+      return char(ord(n - 32))
+    else:
+      return c
+
 def uppercase(str):
-    for n in str:
-        if ord(n) >= 97 and ord(n) <= 122:
-            print("{}".format(ord(n - 32), end=""))
-        else:
-            print("{}".format(n))
+  for n in str:
+    print("{}".format(getUpperChar(n)), end=""))
+
+  print("")
