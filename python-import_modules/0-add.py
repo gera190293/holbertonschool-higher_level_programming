717d1c0d3be61439c549fe22e6a1b908a9248f98
gera190293 <gera190293@gmail.com>
2024-01-25 17:18:18 -0800

    Python - import & modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import add_0
-def main():
+def sum():
     a = 1
     b = 2
     c = add_0.add(a, b)
     print("{} + {} = {}".format(a, b, c))
+
+if __name__ == '__main__':
+    sum()
