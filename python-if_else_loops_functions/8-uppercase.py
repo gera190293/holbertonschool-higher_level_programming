d49e6b8fea432d5d8286b0143e3c4d8cc0c0e193
gera190293 <gera190293@gmail.com>
2024-01-26 18:26:07 -0300

    Python - if/else, loops, functions

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 def getUpperChar(c):
     if ord(c) >= 97 and ord(c) <= 122:
         return chr(ord(c) - 32)
     else:
         return c
+    
 def uppercase(str):
     for n in str:
         print("{}".format(getUpperChar(n)), end="")
