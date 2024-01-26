dc4996a608a61dcba9a519ee005522c0df7a14c5
gera190293 <gera190293@gmail.com>
2024-01-26 08:51:47 -0800

    Python - if/else, loops, functions

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import add_0 as plus_0
-def sum():
+from add_0 import add
+if __name__ == "__main__":
     a = 1
     b = 2
     c = plus_0.add(a, b)
     print("{} + {} = {}".format(a, b, c))
-
-if __name__ == '__main__':
-    sum()
