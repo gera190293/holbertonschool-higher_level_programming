f71443bb116e3a6ab6a23326d17daac482300f49
gera190293 <gera190293@gmail.com>
2024-01-27 14:44:41 -0300

    Python - import & modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif argc == 1:
         print("1 argument:")
-        print("1:{}".format(sys.argv[1]))
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments".format(argc))
         for n in range(1, argc + 1):
-            print("{}:{}".format(n, sys.argv[n]))
+            print("{}: {}".format(n, sys.argv[n]))
