5a438a22272ca230620959030d31a1aa12cf1b26
gera190293 <gera190293@gmail.com>
2024-01-28 05:31:58 -0300

    Python - Data Structures: Lists, Tuples

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 
 def divisible_by_2(my_list=[]):
     new_list = []
-    for n in range(len(my_list)) - 1:
+    for n in range(len(my_list) - 1):
         if my_list[n] % 2 == 0:
             new_list .append("True")
         else:
