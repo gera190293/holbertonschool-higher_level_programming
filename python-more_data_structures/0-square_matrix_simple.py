4187ded7f4c3c1ed2c58100d9a39e37e244e6e24
gera190293 <gera190293@gmail.com>
2024-01-30 12:51:29 -0300

    Python - More Data Structures: Set, Dictionary

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -8,7 +8,8 @@ def copy_matrix(matrix=[[]]):
             sub_matrix.append(matrix[i][j])
         new_matrix.append(sub_matrix)
     return new_matrix
-        
+
+
 def square_matrix_simple(matrix=[]):
     new_matrix = copy_matrix(matrix)
     for i in range(len(matrix)):
