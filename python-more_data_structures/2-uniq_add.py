7f1f8bb32ba663f7668497f112243823efbdf170
gera190293 <gera190293@gmail.com>
2024-01-30 14:15:33 -0300

    Python - More Data Structures: Set, Dictionary

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,6 +4,6 @@ def uniq_add(my_list=[]):
     if not my_list:
         return None
     total = 0
-    for n in range(len(my_list) - 1):
-        total += int(my_list[n])
+    for n in my_list:
+        total += n
     return total
