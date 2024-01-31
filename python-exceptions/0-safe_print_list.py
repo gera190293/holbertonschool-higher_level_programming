08951b8b541cdd734c6b15d6fe02c0e482fab9c8
gera190293 <gera190293@gmail.com>
2024-01-31 17:27:11 -0300

    Python - Exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{i}", end="")
             count += 1
-        except ValueError:
+        except IndexError:
             break
     print()
     return count
