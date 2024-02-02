2ac35e56fed017fe2465c6f1de0988de5085f260
gera190293 <gera190293@gmail.com>
2024-02-02 09:15:28 -0800

    Python - Test-driven development

---
+++ python-if_else_loops_functions/prueba.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for n in "Hola SoY Mayus":
+    # print(f"Character: {n}, ASCII: {ord(n)}")
+    if ord(n) >= 97 and ord(n) <= 122:
+        n = ord(n) - 32
+        print("{}".format(ord(n), end="")
+    else:
+        print("{}".format((n), end="")
+
