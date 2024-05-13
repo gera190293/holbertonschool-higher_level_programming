dedc7607cba23df823500c4d94b0d0cc28b01fdc
gera190293 <gera190293@gmail.com>
2024-05-13 13:10:56 -0300

    Data Structure Exercises

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 
+def chklen(tuple_x=()):
+    if len(tuple_x) >= 2:
+        return tuple_x
+    elif len(tuple_x) == 1:
+        newt = (tuple_x[0], 0)
+        return newt
+    else:
+        newt = (0, 0)
+        return newt
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple(sum(pair) for pair in zip(tuple_a + (0, 0), tuple_b + (0, 0)))
+    t_a = tuple(chklen(tuple_a))
+    t_b = tuple(chklen(tuple_b))
+    a = t_a[0] + t_b[0]
+    b = t_a[1] + t_b[1]
+    new_tuple = (a, b)
+    return new_tuple
