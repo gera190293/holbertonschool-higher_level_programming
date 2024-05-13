35db662e943517dba2c1e91d9bfb9230c269dcb3
gera190293 <gera190293@gmail.com>
2024-05-13 12:16:19 -0300

    If Else Loop Exercises

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for n in range(1, 101):
-        print("Fizz"*(n % 3 == 0) + "Buzz"*( n % 5 == 0) or n, end=" ")
-        
+        print("Fizz" * (n % 3 == 0) + "Buzz" * (n % 5 == 0) or n, end=" ")
