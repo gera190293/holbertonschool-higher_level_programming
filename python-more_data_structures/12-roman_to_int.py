f1738b3b10c416da9a6e5581d164904130322048
gera190293 <gera190293@gmail.com>
2024-05-17 02:21:12 -0300

    More Data Structure Exercises

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -23,7 +23,8 @@ def roman_to_int(roman_string):
     else:
         for i in range(len(roman_string)):
             current_value = roman_value(roman_string[i])
-            if i < len(roman_string) - 1 and current_value < roman_value(roman_string[i + 1]):
+            if (i < len(roman_string) - 1 and 
+                current_value < roman_value(roman_string[i + 1])):
                 number -= current_value
             else:
                 number += current_value
