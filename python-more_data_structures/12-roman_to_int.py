161a4208fd3d76e4ede5599e3fcdd422e48cf98f
gera190293 <gera190293@gmail.com>
2024-05-17 02:19:22 -0300

    More Data Structure Exercises

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 
-def sum(n):
-    if n == "M":
-        return 1000
-    elif n == "D":
-        return 500
-    elif n == "C":
-        return 100
-    elif n == "L":
-        return 50
-    elif n == "X":
-        return 10
-    elif n == "V":
-        return 5
-    else:
-        return 1
-
-
 def roman_to_int(roman_string):
+    def roman_value(symbol):
+        if symbol == "M":
+            return 1000
+        elif symbol == "D":
+            return 500
+        elif symbol == "C":
+            return 100
+        elif symbol == "L":
+            return 50
+        elif symbol == "X":
+            return 10
+        elif symbol == "V":
+            return 5
+        else:
+            return 1
+
     number = 0
-    if roman_string == "" or type(roman_string) != str:
+    if roman_string == "" or not isinstance(roman_string, str):
         return 0
     else:
         for i in range(len(roman_string)):
-            rc = roman_string[i]
-            rv = sum(rc)
-            if i < len(roman_string) - 1 and rv < sum(roman_string[i + 1]):
-                number -= rv
+            current_value = roman_value(roman_string[i])
+            if i < len(roman_string) - 1 and current_value < roman_value(roman_string[i + 1]):
+                number -= current_value
             else:
-                number += rv
+                number += current_value
     return number
