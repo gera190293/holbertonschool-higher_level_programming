c106cad6df778b8a911dba15598d4e9bd4ebb1da
gera190293 <gera190293@gmail.com>
2024-05-23 13:52:01 -0300

    Inheritance excercises

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def lookup(obj):
+    
     """
     Returns the list of available attributes and methods of an object.
 
     :param obj: The object to inspect
     :return: A list of strings representing the names of the attributes and methods
     """
+    
     return dir(obj)
