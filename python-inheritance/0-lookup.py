47fa3eb8163d0da49775e1ec16f50294ea58bf24
gera190293 <gera190293@gmail.com>
2024-05-23 20:34:01 -0300

    Inheritance excercises

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,13 +4,14 @@
 This module provides utilities for inspecting Python objects.
 """
 
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object.
 
     :param obj: The object to inspect.
     :type obj: any
-    :return: A list of strings representing the names of the attributes and methods.
+    :return: A list of strings
     :rtype: list
     """
     return dir(obj)
