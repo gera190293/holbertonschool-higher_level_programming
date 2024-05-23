169566985d4e652edbb90bfdfaebbdeda89d3e10
gera190293 <gera190293@gmail.com>
2024-05-23 11:59:05 -0300

    More classes and objects exercises

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Initialize a new Rectangle instance.
+
+:param width: The width of the rectangle
+:param height: The height of the rectangle
+"""
+
+class Rectangle():
+    
+    """Empty Rectangle class"""
+    
+    pass
