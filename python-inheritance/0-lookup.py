ae85c36dbecad19613b46505cdc082e19f4df295
gera190293 <gera190293@gmail.com>
2024-05-23 20:26:15 -0300

    Inheritance excercises

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+This module provides utilities for inspecting Python objects.
+
+Functions:
+    lookup(obj): Returns a list of available attributes and methods of an object.
+"""
+
 def lookup(obj):
     
     """
