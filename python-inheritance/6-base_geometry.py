95f695fd547ce9253af41a0e2c4659e814b18211
gera190293 <gera190293@gmail.com>
2024-05-24 10:26:30 -0300

    Inheritance excercises

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This module provides utilities for inspecting Python objects.
+"""
+
+
+class BaseGeometry:
+    """empty BaseGeometry class"""
+    pass
+
+
+def area(self):
+    """Public instance to calculate area"""
+    raise "area() is not implemented"
