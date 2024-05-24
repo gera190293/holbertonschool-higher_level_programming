5294ef8db323aa52927034648982d8bb98bf33c9
gera190293 <gera190293@gmail.com>
2024-05-24 10:05:28 -0300

    Inheritance excercises

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This module provides utilities for inspecting Python objects.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """
+    Write a function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from, the specified class
+    otherwise False
+    """
+    
+    return isinstance(obj, a_class)
