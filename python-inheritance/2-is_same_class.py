ce6ea98d4a5d83d2ba05633ddfd725db2f01e1f7
gera190293 <gera190293@gmail.com>
2024-05-24 09:45:31 -0300

    Inheritance excercises

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This module provides utilities for inspecting Python objects.
+"""
+
+def is_same_class(obj, a_class):
+    
+    """
+    Function that returns True if the object is exactly 
+    an instance of the specified class ; otherwise False
+    """
+    
+    
+    return type(obj) is a_class
