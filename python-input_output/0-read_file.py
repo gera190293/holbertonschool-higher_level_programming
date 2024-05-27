99090f6eb201969cd659ec0fc837a5f70e9ed661
gera190293 <gera190293@gmail.com>
2024-05-27 16:50:34 -0300

    Python - Input/Output Exercises

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""This is a Python - Input/Output module exercises"""
+
 def read_file(filename=""):
+    """Function to read a file"""
     with open(filename, 'w', encoding="utf-8") as f:
         filename = f.read()
