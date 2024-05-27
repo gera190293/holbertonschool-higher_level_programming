7db91344c87b01087185e7844eabea5f04ebeac1
gera190293 <gera190293@gmail.com>
2024-05-27 17:42:27 -0300

    Python - Input/Output Exercises

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""This is a Python - Input/Output module exercise."""
+import json
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation of an object"""
+    json.dumps(my_obj)
