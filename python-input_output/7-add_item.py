7b826dfb5083a2b5db3121311dc5eec1341fd20b
gera190293 <gera190293@gmail.com>
2024-05-27 22:10:34 -0300

    Python - Input/Output Exercises

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This is a Python - Input/Output module exercise."""
+import os
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+
+f = add_item.json
+if os.path.exists(f):
+    l = load_from_json_file(f)
+else:
+    l = []
+l.extend(sys.argv[1:])
+save_to_json_file(l, f)
