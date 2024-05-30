d12690ccecf4b5feaf07ed6cbf41e2cbd2fb8605
gera190293 <gera190293@gmail.com>
2024-05-30 17:01:40 -0300

    Serialization exercises

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,10 +6,10 @@ import pickle
 
 def serialize_and_save_to_file(data, filename):
     """Function that serialize and save data to the specified file"""
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'wb') as f:
         pickle.dump(data, f)
 
 def load_and_deserialize(filename):
     """Function that load and deserialize data from a specified file"""
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "rb") as f:
         return pickle.load(f)
