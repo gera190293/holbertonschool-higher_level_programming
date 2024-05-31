026a7ee4c24300487c95ba38ede1f674d7aa5027
gera190293 <gera190293@gmail.com>
2024-05-31 17:33:24 -0300

    RESTful API exercises

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -14,31 +14,35 @@ def fetch_and_print_posts():
     if response.status_code == 200:
         data = response.json()
         
-    for post in data:
+        for post in data:
             print(post['title'])
     else:
-        print("Error to retrieve post")
+        print("Error retrieving posts")
 
 def fetch_and_save_posts():
-    """Function that fetches all post from JSONPlaceholder"""
+    """Function that fetches all post from JSONPlaceholder and saves them into a CSV file"""
     response = requests.get(url)
     print(f"Status Code: {response.status_code}")
 
     if response.status_code == 200:
         data = response.json()
-        names = ['id', 'title', 'body']
-        post_dic = []
+        fieldnames = ['id', 'title', 'body']
+        posts_list = []
 
         for post in data:
-            post_dic = {
-            'id': post['id'], 'title': post['title'], 'body': post['body']
+            post_dict = {
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
             }
-            post_dic.append(post_dic)
+            posts_list.append(post_dict)
 
-        with open('post_dic.csv', mode= 'w', newline='', encoding='utf-8') as f:
-            wr = csv.DictWriter(post_dic, fieldnames=names)
-            wr.writeheader()
-            wr.writerow(post_dic)
-        print("File save susessfull")
+        with open('posts.csv', mode='w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(posts_list)
+        print("File saved successfully as posts.csv")
     else:
-        print("Fail to retrieve post")
+        print("Failed to retrieve posts")
+
+fetch_and_save_posts()
