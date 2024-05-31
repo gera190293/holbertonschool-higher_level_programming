fb24cd75a2943467eb4e56e574bfec24371fd991
gera190293 <gera190293@gmail.com>
2024-05-31 18:20:31 -0300

    RESTful API exercises

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,18 +4,13 @@
 import http.server
 import json
 
-
-class Basic_server(http.server.BaseHTTPRequestHandler):
-    """Class for a basic http server management"""
+class BasicServer(http.server.BaseHTTPRequestHandler):
+    """Class for a basic HTTP server management"""
     def do_GET(self):
         """Method to serve a sample JSON data
         when the endpoint /data is accessed"""
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(b'Hello, this is a simple API!')
-        elif self.path == '/data':
+
+        if self.path == '/data':
             data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
@@ -26,12 +21,19 @@ class Basic_server(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'OK')
+        elif self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
         else:
             self.send_error(404, 'Not Found')
 
 def run_server():
+    """Function to run the server"""
     address = ('', 8000)
-    server = http.server.HTTPServer(address, Basic_server)
+    server = http.server.HTTPServer(address, BasicServer)
+    print('Server running on localhost:8000')
     server.serve_forever()
 
 if __name__ == '__main__':
