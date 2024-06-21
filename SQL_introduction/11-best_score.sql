f15eacfdd62855a5f9c9008468ded23158e87676
gera190293 <gera190293@gmail.com>
2024-06-21 13:01:50 -0300

    SQL Introduction exercises

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- ists all records with a score >= 10 in the table second_table
-SELECT score >= 10, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
