f79b46aef2294a489e0a7ee3f58395ceb8a1a7a2
gera190293 <gera190293@gmail.com>
2024-06-21 17:03:15 -0300

    SQL Introduction exercises

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,2 @@
 -- lists the number of records with the same score in the table second_table
-SELECT score, COUNT(score) AS number
-FROM second_table GROUP BY score
-OREDR BY 2;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score OREDR BY 2;
