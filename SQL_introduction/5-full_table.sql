55a0c0e76d48b535df48cd8c92910f74dd079dd4
gera190293 <gera190293@gmail.com>
2024-06-20 23:04:10 -0300

    SQL Introduction exercises

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,7 @@
 -- prints the following description of a table
 SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_SCHEMA = DATABASE() 
     AND TABLE_NAME = 'first_table';
