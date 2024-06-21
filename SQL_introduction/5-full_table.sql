8b13b45dad8eaed12f7204025b9bf85865c89a84
gera190293 <gera190293@gmail.com>
2024-06-21 00:10:37 -0300

    SQL Introduction exercises

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,13 +1,2 @@
 -- prints the following description of a table
-SELECT 
-    COLUMN_NAME AS 'Column Name', 
-    COLUMN_TYPE AS 'Column Type', 
-    IS_NULLABLE AS 'Is Nullable', 
-    COLUMN_KEY AS 'Key', 
-    COLUMN_DEFAULT AS 'Default', 
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'hbtn_0c_0' AND 
-    TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
