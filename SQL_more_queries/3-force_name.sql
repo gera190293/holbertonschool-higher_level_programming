f70c0ba2759c69b50d3ec9065cd11002b7f78122
gera190293 <gera190293@gmail.com>
2024-06-23 17:21:05 -0300

    More SQL Queries exercises

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,4 @@
--- Creates a table force_name table in the current database,
--- do nothing if the force_name table already exists.
+-- Creates a table force_name table in the current database
 
 CREATE TABLE IF NOT EXISTS `force_name` (
 	`id` INT,
