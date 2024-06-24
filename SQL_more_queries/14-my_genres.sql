ed07d52ae79c3c86da31f649c9998d752beb27d9
gera190293 <gera190293@gmail.com>
2024-06-23 21:06:58 -0300

    More SQL Queries exercises

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
--- Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+-- Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 
   SELECT g.name
     FROM (tv_shows s JOIN tv_show_genres sg ON s.id = sg.show_id)
